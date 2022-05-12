33a518496259b3319dcb6522977efba2674801c1
Rither <rither5711@gmail.com>
2022-05-11 19:04:42 -0500

    busqueda culminada

--- js/app.js
+++ js/app.js
@@ -57,23 +57,28 @@ $year.addEventListener("change",(e)=>{
 
 $minimo.addEventListener("change",(e)=>{
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 })
 
 $maximo.addEventListener("change",(e)=>{
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
 })
 
 $puertas.addEventListener("change",(e)=>{
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas =parseInt(e.target.value);
+    filtrarAuto();
 })
 
 $transmision.addEventListener("change",(e)=>{
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 })
 
 $color.addEventListener("change",(e)=>{
     datosBusqueda.color = e.target.value;
     console.log(datosBusqueda)
+    filtrarAuto();
 })
 
 
@@ -82,6 +87,7 @@ $color.addEventListener("change",(e)=>{
 function mostrarAutos(autos){
     //limpia el html
     limpiarHTML();
+    //itera lo BD autos
     autos.forEach( auto => {
         const {marca, modelo,year, puertas, transmision, color, precio} = auto
         const autoHTML = document.createElement('p');
@@ -105,9 +111,15 @@ function generarYearSelect(){
 
 //filtrar el carro
 function filtrarAuto(){
-    const resultados = autos.filter(filtrarMarca).filter(filtrarYear)
+    const resultados = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMaximo).filter(filtrarMinimo).filter(filtrarPuertas).filter(filtrarTrasmicion).filter(filtrarColor)
     console.log(resultados)
-    mostrarAutos(resultados)
+    if(resultados.length>0){
+        mostrarAutos(resultados)
+    }
+    else{
+        noResultado()
+    }
+
 }
 
 function filtrarMarca(auto){   
@@ -122,4 +134,49 @@ function filtrarYear(auto){
         return auto.year === datosBusqueda.year
     }
     return auto
+}
+
+function filtrarMaximo(auto){
+    if(datosBusqueda.maximo){
+        return auto.precio <= datosBusqueda.maximo
+    }
+    return auto
+}
+
+function filtrarMinimo(auto){
+    if(datosBusqueda.minimo){
+        return auto.precio >= datosBusqueda.minimo
+    }
+    return auto
+}
+
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas){
+        return auto.puertas === datosBusqueda.puertas
+    }
+    return auto
+}
+
+function filtrarTrasmicion(auto){
+    if(datosBusqueda.transmision){
+        return auto.transmision === datosBusqueda.transmision
+    }
+    return auto
+}
+
+function filtrarColor(auto){
+    if(datosBusqueda.color){
+        return auto.color === datosBusqueda.color
+    }
+    return auto
+}
+
+//mensaje en el caso que no haya resultados
+
+function noResultado(){
+    limpiarHTML()
+    const noResultadoHtml = document.createElement('div')
+    noResultadoHtml.classList.add("alerta","error")
+    noResultadoHtml.textContent = "No hay coincidencias, intenta con otros parametros de busqueda"
+    $resultado.appendChild(noResultadoHtml)
 }
